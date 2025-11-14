2fbeb0db1d219fcaffd955dc55d940dbfb8465bf
Andrii Ioffe <andrei.ioffe@gmail.com>
2025-11-14 16:41:45 +0100

    food, death, restart

--- Assignments/Assignment-02/assets/script.js
+++ Assignments/Assignment-02/assets/script.js
@@ -1,3 +1,4 @@
+//enum
 const Direction = Object.freeze({
   Left: { degrees: 270, col: -1, row: 0 },
   Right: { degrees: 90, col: 1, row: 0 },
@@ -5,13 +6,60 @@ const Direction = Object.freeze({
   Down: { degrees: 180, col: 0, row: 1 },
 });
 
-function rotateElement(element, rotation = Direction.Top) {
-  element.style.transform = `rotate(${rotation.degrees}deg)`;
-  currentDirection = rotation;
+// function rotateElement(element, rotation = Direction.Top) {
+//   element.style.transform = `rotate(${rotation.degrees}deg)`;
+//   currentDirection = rotation;
+// }
+
+function generateRandomFoodPosition(headTile, bodyTiles) {
+  let randomRow, randomCol;
+  let isValidPosition = false;
+  while (!isValidPosition) {
+    // Return a random integer between 0 and 15 (both included):
+    randomRow = Math.floor(Math.random() * (ROW_COUNT - 1));
+    randomCol = Math.floor(Math.random() * (COL_COUNT - 1));
+
+    if (
+      randomRow !== headTile.row &&
+      randomCol !== headTile.col &&
+      !bodyTiles.some(
+        (tile) => tile.row === randomRow && tile.col === randomCol
+      )
+    ) {
+      isValidPosition = true;
+    }
+  }
+  return { row: randomRow, col: randomCol };
 }
 
-function gameLoop(gameTiles2d, headTile, bodyTiles) {
-  console.log(desiredDirection);
+function resetGame() {
+  desiredDirection = Direction.Up;
+  currentDirection = Direction.Up;
+  foodTile = { row: null, col: null };
+  headTile = { row: 10, col: 10 };
+  bodyTiles = [
+    { row: 11, col: 10 },
+    { row: 12, col: 10 },
+  ];
+  isGameStarted = false;
+  isGameOver = false;
+  clearInterval(gameLoopInterval);
+}
+function makeNewFood() {
+  if (foodTile.row !== null && foodTile.col !== null) {
+    return;
+  }
+  foodTile = generateRandomFoodPosition(headTile, bodyTiles);
+  gameTiles2d[foodTile.row][foodTile.col].classList.add("state-food");
+}
+function hasColisionWithFood(headTile, foodTile) {
+  return headTile.row === foodTile.row && headTile.col === foodTile.col;
+}
+
+async function gameLoop() {
+  if (!isGameStarted) {
+    return;
+  }
   // determine if can move in desired direction
   let potentialPosition = {
     row: headTile.row + desiredDirection.row,
@@ -19,88 +67,128 @@ function gameLoop(gameTiles2d, headTile, bodyTiles) {
   };
   if (canChangeDirectionToDesired(potentialPosition, bodyTiles)) {
     currentDirection = desiredDirection;
-  }
-  else{
-      // don't change direction - keep moving in current direction
-      potentialPosition = {
-        row: headTile.row + currentDirection.row,
-        col: headTile.col + currentDirection.col,
-      };
+  } else {
+    // don't change direction - keep moving in current direction
+    potentialPosition = {
+      row: headTile.row + currentDirection.row,
+      col: headTile.col + currentDirection.col,
+    };
   }
   updateSnakePosition(potentialPosition, headTile, bodyTiles);
   //rotate head
-  gameTiles2d[headTile.row][headTile.col].style.transform = `rotate(${currentDirection.degrees}deg)`;
-  redrawSnake(gameTiles2d, headTile, bodyTiles);
-  console.log(headTile, bodyTiles);
+
+  redrawGameField(gameTiles2d, headTile, bodyTiles);
+  gameTiles2d[headTile.row][
+    headTile.col
+  ].style.transform = `rotate(${currentDirection.degrees}deg)`;
+  if (isGameOver) {
+    await new Promise((resolve) => setTimeout(resolve, 50));
+    // Show custom alert - click to reset game
+    if (confirm("Game Over! Click OK to play again.")) {
+      resetGame();
+      // Clear all tiles
+      for (let row = 0; row < ROW_COUNT; row++) {
+        for (let col = 0; col < COL_COUNT; col++) {
+          gameTiles2d[row][col].classList.remove(
+            "state-snake-head",
+            "state-snake-body",
+            "state-food"
+          );
+          gameTiles2d[row][col].style.transform = "";
+        }
+      }     
+      //after it gameover fires after escape, enter. Not fire when collision with self. have leftover old body tiles move on the field until they return to body
+    }
+  }
 }
 
 function canChangeDirectionToDesired(potentialPosition, bodyTiles) {
-  existingTile = bodyTiles.find(
-    (tile) =>
-      tile.row === potentialPosition.row && tile.col === potentialPosition.col
-  );
+  // let existingTile = bodyTiles.find(
+  //   (tile) =>
+  //     tile.row === potentialPosition.row && tile.col === potentialPosition.col
+  // );
+
+  const firstBodyTile = bodyTiles[0];
+
+  let isWantToTurnInside =
+    potentialPosition.row === firstBodyTile.row &&
+    potentialPosition.col === firstBodyTile.col;
+
   // if we want to turn into ourself - don't allow
-  if (existingTile === undefined) {
-    return true;
+  if (isWantToTurnInside) {
+    return false;
   }
-  return false;
+  return true;
 }
 function updateSnakePosition(desiredPosition, headTile, bodyTiles) {
   // if (desiredPosition) {
 
-   
-
   // // bodyTiles[bodyTiles.length - 1].row = headTile.row;
   // // bodyTiles[bodyTiles.length - 1].col = headTile.col;
   // }
-  
+
   if (desiredPosition.row < 0) {
     desiredPosition.row = ROW_COUNT - 1;
   }
   if (desiredPosition.col < 0) {
     desiredPosition.col = COL_COUNT - 1;
   }
-    let prevRow = bodyTiles[0].row; // cached
-    let prevCol = bodyTiles[0].col;
-    bodyTiles[0].col = headTile.col;
-    bodyTiles[0].row = headTile.row;
-
-    let oldCol = prevCol;
-    let oldRow = prevRow;
-    for (let i = 1; i < bodyTiles.length; i++) {
-      prevRow = bodyTiles[i].row;
-      prevCol = bodyTiles[i].col;
-      bodyTiles[i].row = oldRow;
-      bodyTiles[i].col = oldCol;
-      oldRow = prevRow;
-      oldCol = prevCol;
-    }
+  let prevRow = bodyTiles[0].row; // cached
+  let prevCol = bodyTiles[0].col;
+  bodyTiles[0].col = headTile.col;
+  bodyTiles[0].row = headTile.row;
 
   headTile.row = desiredPosition.row % ROW_COUNT;
   headTile.col = desiredPosition.col % COL_COUNT;
+
+  let oldCol = prevCol;
+  let oldRow = prevRow;
+  for (let i = 1; i < bodyTiles.length; i++) {
+    prevRow = bodyTiles[i].row;
+    prevCol = bodyTiles[i].col;
+    bodyTiles[i].row = oldRow;
+    bodyTiles[i].col = oldCol;
+    oldRow = prevRow;
+    oldCol = prevCol;
+  }
+  if (hasColisionWithFood(headTile, foodTile)) {
+    foodTile = { row: null, col: null };
+    //add new body tile at the end
+    bodyTiles.push({ row: oldRow, col: oldCol });
+    makeNewFood();
+  }
+  //collision with self - defeat
+  if (
+    bodyTiles.some(
+      (tile) => tile.row === headTile.row && tile.col === headTile.col
+    )
+  ) {
+    isGameOver = true;
+  }
 }
-function redrawSnake(gameTiles2d, headTile, bodyTiles) {
+function redrawGameField(gameTiles2d, headTile, bodyTiles) {
   for (let row = 0; row < ROW_COUNT; row++) {
     for (let col = 0; col < COL_COUNT; col++) {
+      // Clear all state classes first
+      gameTiles2d[row][col].classList.remove(
+        "state-snake-head",
+        "state-snake-body",
+        "state-food"
+      );
+
       if (row === headTile.row && col === headTile.col) {
-        gameTiles2d[row][col].classList.add("snake-head");
-        gameTiles2d[row][col].classList.remove("snake-body"); // change to state
-        continue;
+        gameTiles2d[row][col].classList.add("state-snake-head");
       } else {
         const bodyTile = bodyTiles.find(
           (tile) => tile.row === row && tile.col === col
         );
         if (bodyTile !== undefined) {
-          gameTiles2d[row][col].classList.add("snake-body");
-          gameTiles2d[row][col].classList.remove("snake-head"); // change to state
-          continue;
+          gameTiles2d[row][col].classList.add("state-snake-body");
         }
       }
-
-      gameTiles2d[row][col].classList.remove("snake-head");
-      gameTiles2d[row][col].classList.remove("snake-body");
     }
   }
+  gameTiles2d[foodTile.row][foodTile.col].classList.add("state-food");
 }
 
 // grid 64 / 64
@@ -114,7 +202,7 @@ let currentDirection = Direction.Up;
 // const bodyTiles = [{ row: null, col: null }];
 
 let headTile = { row: 10, col: 10 };
-const bodyTiles = [
+let bodyTiles = [
   { row: 11, col: 10 },
   { row: 12, col: 10 },
 ];
@@ -145,7 +233,10 @@ for (let row = 0; row < ROW_COUNT; row++) {
 // gameTiles2d[0][2].classList.add("snake-body");
 // rotateElement(gameTiles2d[0][0], Direction.Left);
 
+let foodTile = { row: null, col: null };
 let gameLoopInterval = null;
+let isGameStarted = false;
+let isGameOver = false;
 
 document.addEventListener("keydown", (event) => {
   switch (event.key) {
@@ -163,15 +254,18 @@ document.addEventListener("keydown", (event) => {
       desiredDirection = Direction.Down;
       break;
     case "Enter":
-      gameLoopInterval = setInterval(
-        gameLoop,
-        300,
-        gameTiles2d,
-        headTile,
-        bodyTiles
-      ); // move every 200ms
+      if (isGameStarted) {
+        break;
+      }
+      isGameStarted = true;
+      makeNewFood();
+      gameLoopInterval = setInterval(gameLoop, 300); // move every 200ms
       break;
     case "Escape":
+      if (!isGameStarted) {
+        break;
+      }
+      isGameStarted = false;
       clearInterval(gameLoopInterval);
       break;
   }
